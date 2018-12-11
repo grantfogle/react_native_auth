b1428420ee7ab80ed009fb6865145f8cf634b0f2
grantfogle <grantjfogle@gmail.com>
2018-12-10 17:25:38 -0700

    adding form to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+
 
 class App extends Component {
+
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyAqYIDo33m2_jUcx66K6YiZicgs1v2EwOc',
+            authDomain: 'authentication-react-nat-a28dc.firebaseapp.com',
+            databaseURL: 'https://authentication-react-nat-a28dc.firebaseio.com',
+            projectId: 'authentication-react-nat-a28dc',
+            storageBucket: 'authentication-react-nat-a28dc.appspot.com',
+            messagingSenderId: '839429482018'
+        });
+    }
+
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <Text>An app</Text>
+                <LoginForm />
             </View>
         );
     }
