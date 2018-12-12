3eb4f86c9d517306e5d0db4ca5bf7c635d815e72
grantfogle <grantjfogle@gmail.com>
2018-12-12 14:29:42 -0700

    updating authentication credentials

--- src/App.js
+++ src/App.js
@@ -8,14 +8,15 @@ import LoginForm from './components/LoginForm';
 class App extends Component {
 
     componentWillMount() {
-        firebase.initializeApp({
-            apiKey: 'AIzaSyAqYIDo33m2_jUcx66K6YiZicgs1v2EwOc',
-            authDomain: 'authentication-react-nat-a28dc.firebaseapp.com',
-            databaseURL: 'https://authentication-react-nat-a28dc.firebaseio.com',
-            projectId: 'authentication-react-nat-a28dc',
-            storageBucket: 'authentication-react-nat-a28dc.appspot.com',
-            messagingSenderId: '839429482018'
-        });
+        var config = {
+            apiKey: config.apiKey,
+            authDomain: config.authDomain,
+            databaseURL: config.databaseURL,
+            projectId: config.projectId,
+            storageBucket: config.storageBucket,
+            messagingSenderId: config.messagingSenderId
+        };
+        firebase.initializeApp(config);
     }
 
     render() {
